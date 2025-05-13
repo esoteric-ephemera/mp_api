229ed0843faa46852b4c485e131c30b99b16288d
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-05-13 12:31:55 -0700

    draft trajectory reading

--- mp_api/client/routes/materials/tasks.py
+++ mp_api/client/routes/materials/tasks.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from datetime import datetime
 
+import pandas as pd
+import pyarrow.parquet as pq
+from emmet.core.mpid import MPID
 from emmet.core.tasks import TaskDoc
+from emmet.core.trajectory import Trajectory
 
-from mp_api.client.core import BaseRester, MPRestError
+from mp_api.client.core import BaseRester
 from mp_api.client.core.utils import validate_ids
 
 
@@ -13,25 +18,41 @@ class TaskRester(BaseRester[TaskDoc]):
     document_model = TaskDoc  # type: ignore
     primary_key = "task_id"
 
-    def get_trajectory(self, task_id):
+    def get_trajectory(self, task_ids: str | MPID | list[str | MPID]):
         """Returns a Trajectory object containing the geometry of the
         material throughout a calculation. This is most useful for
         observing how a material relaxes during a geometry optimization.
 
         Args:
-            task_id (str): Task ID
+            task_ids (str or MPID, or list of str or MPID): Task IDs
 
         """
-        traj_data = self._query_resource_data(
-            {"task_ids": [task_id]}, suburl="trajectory/", use_document_model=False
-        )[0].get(
-            "trajectories", None
-        )  # type: ignore
+        if isinstance(task_ids, str | MPID):
+            task_ids = [task_ids]
+        task_ids = [str(task_id) for task_id in task_ids]
 
-        if traj_data is None:
-            raise MPRestError(f"No trajectory data for {task_id} found")
+        manifest = pd.read_parquet(
+            "s3://materialsproject-parsed/trajectories/manifest.parquet",
+            columns=["task_id", "path"],
+            filters=[("task_id", "in", task_ids)],
+        )
+        required_paths = defaultdict(list)
+        for task_id in task_ids:
+            if isinstance(
+                path := manifest[manifest.task_id == task_id].path.squeeze(), str
+            ):
+                required_paths[path].append(task_id)
 
-        return traj_data
+        trajs = {}
+        for path, tids in required_paths.items():
+            _new_data = pq.read_table(
+                f"s3://materialsproject-parsed/{path}",
+                filters=[("identifier", "in", tids)],
+            )
+            for tid in tids:
+                trajs[tid] = Trajectory.from_arrow(_new_data, identifier=tid)
+
+        return trajs
 
     def search(
         self,
